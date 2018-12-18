93e877699a0aeb4a05f3d169a9a43abd1d238209
Onra77 <onra77@hotmai.com>
2018-12-18 16:02:22 +0100

    added contact, about with links to and from welcome.

--- resources/views/contact.php
+++ resources/views/contact.php
@@ -4,18 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Contact - R&R Webshop</title>
 </head>
 <body>
     
-    <h1>R&M Webshop</h1>
+    <h1>Contact</h1>
 
-    <ul>
-    <li><a href="">Home</a></li>
+<ol>
+    <li><a href="welcome">Home</a></li>
     <li>Neem <a href="/contact">contact</a> met ons voor meer informatie.</li>
     <li>Over <a href="/about">mij?</a></li>
-
-
+</ol>
 
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/layout', function () {
+    return view('layout');
+});
