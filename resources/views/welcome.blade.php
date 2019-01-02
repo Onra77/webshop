1dfb6efd1609780d66fcbf6801df87f1e3424619
Onra77 <onra77@hotmail.com>
2019-01-02 15:34:53 +0100

    Sending Data to your Views, failed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 @extends('layout')
-
-@section('title')
-
-    Home - Webshop 
-
-@endsection 
-
-
+@section('title', 'Home - Webshop') 
 @section('content')
 
 <div id="blog">
 <h1>Welkom!</h1>
+
+<ul>
+    @foreach ($tasks as $task)
+        <li>({ $task })</li>
+    @endforeach
+</ul>
+
 </div>
 
 @endsection
